ead537edc1c8bff736a816fd5366be5547814db8
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-08-31 14:10:09 -0700

    Additional functionality added to resolvers

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,10 +1,15 @@
+
+//Here, we import "GraphQLError" from "graphql", which will be used to handle "Authentication" "Error"s (defined below). We also import the built-in "jwt" (stands for "JSON Web Tokens") package from "jsonwebtoken", to handle the creation and "authentication" or JSON Web Tokens.
 const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
+//For this app, we have used a simple string for the "secret", which is used to assist in verification of the "jwt"'s "authentication". Whatever the key is, it is important to keep this information from being exposed during transactions.
+
+//We also declare that the "Token" will "expir"e "2h"ours after it is issued.
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
+//Here, we provide "boilerplate auth" code blocks, common to this sort of app. Witout these basic blocks, the app could not authenticate "user"s upon log-in.
 module.exports = {
   AuthenticationError: new GraphQLError('Could not authenticate user.', {
     extensions: {
