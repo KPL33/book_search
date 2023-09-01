59c234b169cff26f674f360076dfbc78e57795e4
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-09-01 15:02:30 -0700

    Notes added to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css';
 
+//Here, we import the "Outlet" component, which will helpu us render the components of the current route.
 import { Outlet } from 'react-router-dom';
+
+//Here, we import the "Apollo" components necessary for interaction with our GraphQL API.
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import Navbar from './components/Navbar';
 
+//Here, we define a "new" instance of the "ApolloClient" that we imported above. We designate the "url" of the "graphql" server through which our app will access its data. We will manage queries and store them as a "new" instance of the "InMemoryCache" component we imported above.
 const client = new ApolloClient({
   url: '/graphql',
   cache: new InMemoryCache()
 });
 
+//Here, we give the app the abiity to "return" the contents of the "Navbar" and "Outlet" compnents". We wrap the entire app in an "ApolloProvider", which provides the "ApolloClient" instance to all components in our React app.
 function App() {
   return (
     <ApolloProvider client={client}>
@@ -19,4 +24,5 @@ function App() {
   );
 }
 
+//Here, we export this code for use elsewhere in our app.
 export default App;
